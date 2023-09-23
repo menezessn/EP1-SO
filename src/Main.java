3ffda79d4de47ee9fe5c11d4bbc37765847551f9
Marcelo de Menezes Nascimento <mar_chelo10@hotmail.com>
2023-09-23 18:14:14 -0300

    Inicio da implementacao do escalonador

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,13 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("ESCALONADOR");
+
         int quantum =  QuantumReader.readQuantum("programas/");
+
+        //Excluindo o arquivo de log, caso ele exista, para que nao haja conflito
+        DeleteLogFile.LogFileDelete(quantum);
+
+
         System.out.println(quantum); //numero de quantuns
 
         List<BCP> programs = BCPTableCreator.createBCPTable("programas/"); // Criando lista de processos
@@ -21,6 +27,42 @@ public class Main {
             LogFileWriter.writeLogFile("Carregando " + program.getName());
             System.out.println("Carregando " + program.getName());
         }
+
+        //while(ready.size()>0){
+            BCP runningProcess = ready.poll();
+            runningProcess.setProcessState(Estado.EXECUTANDO);
+
+            for (int i = 0; i < quantum; i++){
+                String program[] = runningProcess.getProgram();
+                String instruction = program[runningProcess.getPC()];
+                if (instruction.contains("X=")){
+                    int assignment = Integer.parseInt(instruction.replace("X=", ""));
+                    System.out.println(assignment);
+                    runningProcess.setX(assignment);
+                }
+                if (instruction.contains("Y=")){
+                    int assignment = Integer.parseInt(instruction.replace("Y=", ""));
+                    runningProcess.setX(assignment);
+                }
+                if(instruction.equals("E/S")){
+                    runningProcess.setProcessState(Estado.BLOQUEADO);
+                    blocked.offer(runningProcess);
+                    break;
+                }
+                if(instruction.equals("COM")){
+                    System.out.println("COM");
+                }
+                if(instruction.equals("SAIDA")){
+                    runningProcess.setProcessState(Estado.BLOQUEADO);
+                    runningProcess.setPC(-1);
+                    programs.remove(runningProcess);
+                    break;
+                }
+                runningProcess.setPC(runningProcess.getPC()+1);
+
+            }
+
+        //}
 
 
 
